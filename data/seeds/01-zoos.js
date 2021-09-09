958eed83177db4dbe3dbf3f7770cec05078f62c3
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-09 19:24:17 +0200

    wip

--- data/seeds/01-zoos.js
+++ data/seeds/01-zoos.js
@@ -1,6 +1,7 @@
 
 exports.seed = function(knex, Promise) {
-  return knex('zoos').insert([   
+  // we are not trucating here anymore
+  return knex('zoos').insert([
     { zoo_name: 'San Diego Zoo', address: "2920 Zoo Dr, San Diego, CA 92101" },
     { zoo_name: 'St. Louis Zoo', address: "Government Dr, St. Louis, MO 63110" }
   ]);
